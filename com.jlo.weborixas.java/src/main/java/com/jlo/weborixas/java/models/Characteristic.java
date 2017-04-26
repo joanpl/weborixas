51ab8d1ae0a4199341f4f46c787b80e5cb891288
joanpl <joanpl@gmail.com>
2017-04-26 16:55:42 +0100

    link characteristic with Dynamo

--- com.jlo.weborixas.java/src/main/java/com/jlo/weborixas/java/models/Characteristic.java
+++ com.jlo.weborixas.java/src/main/java/com/jlo/weborixas/java/models/Characteristic.java
@@ -1,12 +1,18 @@
 package com.jlo.weborixas.java.models;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "characteristic")
 public class Characteristic {
 	
 	private Orixa orixa;
 	
-	private String name;
+	private String orixaName;
 	
-	private String question;
+	@DynamoDBHashKey
+	private String attribute;
 	
 	private String description;
 	
@@ -19,22 +25,25 @@ public class Characteristic {
 		this.orixa = orixa;
 	}
 
-	public String getName() {
-		return name;
+	@DynamoDBAttribute
+	public String getOrixaName() {
+		return orixaName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setOrixaName(String name) {
+		this.orixaName = name;
 	}
 
-	public String getQuestion() {
-		return question;
+	@DynamoDBAttribute
+	public String getAttribute() {
+		return attribute;
 	}
 
-	public void setQuestion(String question) {
-		this.question = question;
+	public void setAttribute(String question) {
+		this.attribute = question;
 	}
 
+	@DynamoDBAttribute
 	public String getDescription() {
 		return description;
 	}
